16dca7364301158defcb841f4e6e1335c8ac38c3
Daniil Markov <mrmrkda@gmail.com>
2024-10-17 18:53:09 +1100

    chore: run `nfc-manager` example

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
 import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
+import { Text, TouchableOpacity, View } from "react-native"
+import NfcManager, { NfcTech } from "react-native-nfc-manager"
+
+// Pre-step, call this before any NFC operations
+NfcManager.start()
 
 export default function App() {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef)
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag()
+      console.warn("Tag found", tag)
+    } catch (ex) {
+      console.warn("Oops!", ex)
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest()
+    }
+  }
+
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text>Open up App.tsx to start working on your app!</Text>
+      <TouchableOpacity
+        className={"border rounded-full mt-8 p-4"}
+        onPress={readNdef}
+      >
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   )
