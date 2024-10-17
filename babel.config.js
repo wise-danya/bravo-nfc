b329c8273da121aac6988f53081e988432f0edc2
Daniil Markov <mrmrkda@gmail.com>
2024-10-18 08:43:27 +1100

    chore: configs

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true)
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel",
+      [
+        "module-resolver",
+        {
+          extensions: [".ts", ".tsx"],
+          root: "./src",
+          alias: {
+            app: "./src/app",
+            screens: "./src/screens",
+            features: "./src/features",
+            shared: "./src/shared",
+          },
+        },
+      ],
+    ],
   }
 }
